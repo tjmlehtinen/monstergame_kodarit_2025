8998c29fa564df1c553ed0f41affe9b242e0d40a
Tommi Lehtinen <tommijmlehtinen@gmail.com>
2025-02-03 18:28:33 +0200

    kolmas tunti eli 03022025

--- public/script.js
+++ public/script.js
@@ -1,5 +1,20 @@
+const startScreen = document.getElementById("start-screen");
+const gameScreen = document.getElementById("game-screen");
+
+const BOARDSIZE = 15;
+let gameBoard;
+
+
 document.getElementById("start-button").addEventListener("click", startGame);
 
 function startGame() {
-    console.log("nappi toimii");
+    startScreen.style.display = "none";
+    gameScreen.style.display = "block";
+    gameBoard = generateRandomBoard();
+    console.log(gameBoard);
+}
+
+function generateRandomBoard() {
+    const newBoard = Array.from({ length: BOARDSIZE }, () => Array(BOARDSIZE).fill(" "));
+    return newBoard;
 }
